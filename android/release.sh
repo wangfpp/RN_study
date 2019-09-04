3e15d7f8069c5d847a773a7416cd8a3307f93d7b
wangfpp <wangfpp@gmail.com>
2019-09-04 11:29:19 +0800

    changed: 修改构建脚本

--- android/release.sh
+++ android/release.sh
@@ -3,20 +3,41 @@
 # 自动化打包生成app和version.json的版本信息
 # 1. 打包APP
 # 2.  生成changedlog
-# ./buildapp.sh $1 #采用已有的打包脚本进行打包
-# ./tag2log.sh
+
+
+#采用已有的打包脚本进行打包
+./buildapp.sh $1
+
+#更新文件的URL
 UPDATEURL="http://172.16.1.61:2081/html/app/shiyin/"
 
-packageversion=$(cat ../package.json | jq '.version') # jq读取JSON字段
+# jq读取JSON字段
+packageversion=$(cat ../package.json | jq '.version')
 echo $packageversion
-VERSION=$(echo $packageversion| sed $'s/\"//g')
 #sed 去除引号
 # echo "$opt" | tr -d '"' 去除引号
-appNameWithFolder=$(find ./app/build/outputs/apk/release/*$VERSION*.apk -ctime -1) #ls ./app/build/outputs/apk/release/*.apk -t | head -1
+VERSION=$(echo $packageversion| sed $'s/\"//g')
+
+#ls ./app/build/outputs/apk/release/*.apk -t | head -1
+appNameWithFolder=$(find ./app/build/outputs/apk/release/*$VERSION*.apk -ctime -1)
+
+#去除目录前缀
 appName=${appNameWithFolder##*/}
 if [ $appName ]; then
     echo $appName
-    $(echo {"version": $packageversion,"filename": $appName,"url": $UPDATEURL$appName,"desc":[]} >  ./app/build/outputs/apk/release/version.json)
+    JSON="{\"version\": $packageversion,\"filename\": \"$appName\",\"url\": \"$UPDATEURL$appName\",\"desc\":[]}"
+    echo  $JSON >  ./app/build/outputs/apk/release/version.json
+    echo $appName
+    # 获取字符串的len
+    strlen=$(echo ${#appName})
+
+    # 掐头去尾截取的长度
+    truelen=$(expr $strlen - 11)
+
+    # 截取字符串
+    TAGNAME=$(echo ${appName:  7 : $truelen })
+    echo $TAGNAME
+    ./tag2log.sh $TAGNAME
 else
     echo -e "\033[31m 未找到含有版本号$packageversion的APK\033[0m"
     exit 1
--- android/tag2log.sh
+++ android/tag2log.sh
@@ -1,27 +1,66 @@
 # !/bin/bash
+currTag=$1
+
+
 function commitList() {
-    keyword=$1
-    commits=$(git log $lastTag --date=short --pretty=format:" - %s %b  提交人及邮箱 %an-%ae commitID: %h 日期:%ad" --grep=^$keyword)
+    lastTag=$1
+    formtag=$2
+    keyword=$3
+    local commits
+    if [[ $# == 2 ]];then
+        commits=$(git log $1 --date=short --pretty=format:" - %s %b  提交人及邮箱 %an－%ae commitID: %h 日期:%ad" --grep=^$2)
+    elif [[ $# == 3 ]];then
+        echo "当前tag: $2"
+        commits=$(git log $1..$2 --date=short --pretty=format:" - %s %b  提交人及邮箱 %an－%ae commitID: %h 日期:%ad" --grep=^$3)
+    fi
     echo  "$commits" >> commits.md
 }
 
 
+# 获取最后一次tag
+lastTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 
-lastTag=$(git describe --tags `git rev-list --tags --max-count=1`) # 获取最后一次tag
-echo $?
-echo "上次的tag $lastTag"
+echo "上次的tag: $lastTag"
 echo '## 新增内容' > commits.md
 # commits=$(git log $lastTag --date=short --pretty=format:" - %s   commitID: %h 日期:%ad" --grep=^$1) # 新特性
-commitList "add" #新特性
+
+
+if [ $currTag ];then
+    git tag -a $currTag -m "打包版本号$currTag"
+    git push origin $currTag
+fi
+
+
+if [ $currTag ];then
+    commitList  $lastTag $currTag "add"
+else 
+    commitList  $lastTag  "add"
+fi
 
 echo '## 废弃内容' >>  commits.md
-commitList "del:"  #废弃功能
+
+if [ $currTag ];then
+    commitList  $lastTag $currTag "del:"
+else 
+    commitList  $lastTag  "del:"
+fi
 
 echo '## 修改项' >>  commits.md
-commitList "changed"  #修改项
+
+if [ $currTag ];then
+    commitList $lastTag $currTag "changed"
+else 
+    commitList $lastTag "changed"
+fi
 
 echo '## 修复内容' >>  commits.md
-commitList "bugfix"  #修改项
+
+if [ $currTag ];then
+    commitList $lastTag $currTag "bugfix"
+else 
+    commitList $lastTag "bugfix"
+fi
+
 
 
 # echo  "$commits" >> commits.md
