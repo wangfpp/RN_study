4512b031647d6654f87d49fc8d3728386c31e6c2
wangfpp <wangfpp@gmail.com>
2019-09-16 11:47:43 +0800

    add: 增加历史ｉｐ功能

--- view/setip.js
+++ view/setip.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import { View, TextInput, StyleSheet, Text} from 'react-native';
+import { View, TextInput, StyleSheet, Text, ScrollView, TouchableHighlight} from 'react-native';
 import { connect } from 'react-redux';
 import Ionicons from 'react-native-vector-icons/AntDesign';
 import DeviceInfo from 'react-native-device-info';
 
 
-export default connect(state => {
-	return {
-		siteIP: state.setipreducer.ip
-	}
-})(class SetIP extends Component {
+class SetIP extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            ip: this.props.siteIP,
+            ip: props.siteIP,
+            iplist: props.iplist,
             icon:  {
                 name: 'checkcircleo',
                 color: 'green'
@@ -21,22 +18,19 @@ export default connect(state => {
             phoneNumber: '',
             mac: ''
         };
-        console.log(this);
     }
     async componentDidMount() {
-        console.log(111)
         let mac = await DeviceInfo.getMACAddress();
         let phonenum = await DeviceInfo.getPhoneNumber();
-        console.log(phonenum)
         this.setState({
             mac
         })
-        DeviceInfo.getIPAddress().then(ip => {
-            this.setState({
-                ip: ip ? ip + ':8887' : this.props.siteIP
-            })
-            this.changeStoreIP()
-          });
+        // DeviceInfo.getIPAddress().then(ip => {
+        //     this.setState({
+        //         ip: ip ? ip + ':8887' : this.props.siteIP
+        //     })
+        //     this.changeStoreIP()
+        // });
     }
     changeip(text) {
         let urlpattern = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\:([0-9]|[1-9]\d{1,3}|[1-5]\d{4}|6[0-5]{2}[0-3][0-5])$/;
@@ -59,33 +53,70 @@ export default connect(state => {
         }
         
     };
-    changeStoreIP() {
-        this.props.dispatch({
-            type: 'SetIP',
-            ip: this.state.ip
-        });
+    changeOnEnd() {
+        this.props.changeStoreIP(this.state.ip);
+        this.props.changeIpList(this.state.ip);
     };
-
+    tapHistoryIP(ip) { // 点击历史记录
+        this.props.changeStoreIP(ip);
+        this.setState({
+            ip: ip
+        })
+    }
     render() {
         return (
             <View style={styles.conatiner}>
-                    <View style={styles.inputcontent}>
-                        <TextInput 
-                        placeholder="请输入WS的ip"
-                        value={this.state.ip}
-                        returnKeyLabel="确定"
-                        onSubmitEditing={ this.changeStoreIP.bind(this) }
-                        onChangeText={this.changeip.bind(this)}
-                         />
-                        <Ionicons name={this.state.icon.name} color={this.state.icon.color} size={26}></Ionicons>
+                    <View>
+                        <View style={styles.inputcontent}>
+                            <TextInput 
+                            placeholder="请输入WS的ip"
+                            value={this.state.ip}
+                            returnKeyLabel="确定"
+                            onSubmitEditing={ this.changeOnEnd.bind(this) }
+                            onChangeText={this.changeip.bind(this)}
+                            />
+                            <Ionicons name={this.state.icon.name} color={this.state.icon.color} size={26}></Ionicons>
+                        </View>
                     </View>
-                    <Text>MAC地址: {this.state.mac}</Text>
-                    <Text>手机号:  {this.state.phoneNumber}</Text>
+                    <View style={{flex:1}}>
+                        <Text　style={{textAlign: "center", fontSize: 20, fontWeight: "bold"}}>历史IP 点击切换当前IP</Text>
+                        <ScrollView style={{flex:1}}>
+                        {
+                            this.props.iplist.map((item, index) => {
+                                return (
+                                    item ? <TouchableHighlight key={index} style={{height: 40, display: "flex", justifyContent: "center", alignItems: "center"}} underlayColor="tomato" onPress={this.tapHistoryIP.bind(this, item)}>
+                                        <Text  style={{marginBottom: 5, fontSize: 18}}>{item}</Text>
+                                    </TouchableHighlight> : null
+                                    
+                                )
+                            })
+                        }
+                        </ScrollView>
+                    </View>
             </View>
-        );
+        )
     }
-});
+};
 
+const mapStateToProps = (state) => {
+    return {
+        iplist: state.iplist,
+        siteIP: state.setipreducer.ip
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        changeStoreIP: ip => {
+            dispatch({type: 'SetIP', ip: ip});
+        },
+        changeIpList:  ip => {
+            dispatch({type: '', ip});
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SetIP);
 const styles = StyleSheet.create({
     conatiner: {
         flex:1,
